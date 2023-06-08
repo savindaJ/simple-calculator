57904c35d536f84ae11168fc6f6828332ce85337
SavindaJayasekara <thantrige32@gmail.com>
2023-06-08 18:27:13 +0530

    create button 1

--- window.py
+++ window.py
@@ -1,240 +1,256 @@
 from tkinter import *
+import tkinter as tk
 
 
 def btn_clicked():
+    # entry0.
     print("Button Clicked")
 
 
 window = Tk()
 
+v = tk.StringVar()
+
+
+def set_text(value):
+    # text1=entry1.get()
+    entry1.insert(0, value)
+    # v.set(word)
+
+
+def button_clicked():
+    set_text("1")
+
+
 window.geometry("350x500")
-window.configure(bg = "#ffffff")
+window.configure(bg="#ffffff")
 canvas = Canvas(
     window,
-    bg = "#ffffff",
-    height = 500,
-    width = 350,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge")
-canvas.place(x = 0, y = 0)
+    bg="#ffffff",
+    height=500,
+    width=350,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge")
+canvas.place(x=0, y=0)
 
-background_img = PhotoImage(file = f"background.png")
+background_img = PhotoImage(file=f"background.png")
 background = canvas.create_image(
     175.0, 250.0,
     image=background_img)
 
-img0 = PhotoImage(file = f"img0.png")
+img0 = PhotoImage(file=f"img0.png")
 b0 = Button(
-    image = img0,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img0,
+    borderwidth=0,
+    highlightthickness=0,
+    command=button_clicked,
+    relief="flat")
 
 b0.place(
-    x = 25, y = 200,
-    width = 87,
-    height = 39)
+    x=25, y=200,
+    width=87,
+    height=39)
 
-img1 = PhotoImage(file = f"img1.png")
+img1 = PhotoImage(file=f"img1.png")
 b1 = Button(
-    image = img1,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b1.place(
-    x = -330, y = -34,
-    width = 87,
-    height = 39)
+    x=-330, y=-34,
+    width=87,
+    height=39)
 
-img2 = PhotoImage(file = f"img2.png")
+img2 = PhotoImage(file=f"img2.png")
 b2 = Button(
-    image = img2,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b2.place(
-    x = -223, y = -35,
-    width = 87,
-    height = 39)
+    x=-223, y=-35,
+    width=87,
+    height=39)
 
-img3 = PhotoImage(file = f"img3.png")
+img3 = PhotoImage(file=f"img3.png")
 b3 = Button(
-    image = img3,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b3.place(
-    x = -116, y = -34,
-    width = 87,
-    height = 39)
+    x=-116, y=-34,
+    width=87,
+    height=39)
 
-img4 = PhotoImage(file = f"img4.png")
+img4 = PhotoImage(file=f"img4.png")
 b4 = Button(
-    image = img4,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img4,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b4.place(
-    x = 140, y = 200,
-    width = 87,
-    height = 39)
+    x=140, y=200,
+    width=87,
+    height=39)
 
-img5 = PhotoImage(file = f"img5.png")
+img5 = PhotoImage(file=f"img5.png")
 b5 = Button(
-    image = img5,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img5,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b5.place(
-    x = 250, y = 200,
-    width = 87,
-    height = 39)
+    x=250, y=200,
+    width=87,
+    height=39)
 
-img6 = PhotoImage(file = f"img6.png")
+img6 = PhotoImage(file=f"img6.png")
 b6 = Button(
-    image = img6,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img6,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b6.place(
-    x = 25, y = 270,
-    width = 87,
-    height = 39)
+    x=25, y=270,
+    width=87,
+    height=39)
 
-img7 = PhotoImage(file = f"img7.png")
+img7 = PhotoImage(file=f"img7.png")
 b7 = Button(
-    image = img7,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img7,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b7.place(
-    x = 140, y = 270,
-    width = 87,
-    height = 39)
+    x=140, y=270,
+    width=87,
+    height=39)
 
-img8 = PhotoImage(file = f"img8.png")
+img8 = PhotoImage(file=f"img8.png")
 b8 = Button(
-    image = img8,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img8,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b8.place(
-    x = 250, y = 270,
-    width = 87,
-    height = 41)
+    x=250, y=270,
+    width=87,
+    height=41)
 
-img9 = PhotoImage(file = f"img9.png")
+img9 = PhotoImage(file=f"img9.png")
 b9 = Button(
-    image = img9,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img9,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b9.place(
-    x = 25, y = 340,
-    width = 87,
-    height = 41)
+    x=25, y=340,
+    width=87,
+    height=41)
 
-img10 = PhotoImage(file = f"img10.png")
+img10 = PhotoImage(file=f"img10.png")
 b10 = Button(
-    image = img10,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img10,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b10.place(
-    x = 140, y = 340,
-    width = 87,
-    height = 41)
+    x=140, y=340,
+    width=87,
+    height=41)
 
-img11 = PhotoImage(file = f"img11.png")
+img11 = PhotoImage(file=f"img11.png")
 b11 = Button(
-    image = img11,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img11,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b11.place(
-    x = 250, y = 340,
-    width = 87,
-    height = 41)
+    x=250, y=340,
+    width=87,
+    height=41)
 
-img12 = PhotoImage(file = f"img12.png")
+img12 = PhotoImage(file=f"img12.png")
 b12 = Button(
-    image = img12,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img12,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b12.place(
-    x = 180, y = 425,
-    width = 87,
-    height = 39)
+    x=180, y=425,
+    width=87,
+    height=39)
 
-img13 = PhotoImage(file = f"img13.png")
+img13 = PhotoImage(file=f"img13.png")
 b13 = Button(
-    image = img13,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    image=img13,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
 
 b13.place(
-    x = 80, y = 425,
-    width = 87,
-    height = 39)
+    x=80, y=425,
+    width=87,
+    height=39)
 
-entry0_img = PhotoImage(file = f"img_textBox0.png")
+entry0_img = PhotoImage(file=f"img_textBox0.png")
 entry0_bg = canvas.create_image(
     -179.5, -143.5,
-    image = entry0_img)
+    image=entry0_img)
 
 entry0 = Entry(
-    bd = 0,
-    bg = "#ffffff",
-    highlightthickness = 0)
+    bd=0,
+    bg="#ffffff",
+    highlightthickness=0)
 
 entry0.place(
-    x = 40.0, y = 120,
-    width = 281.0,
-    height = 35)
+    x=40.0, y=120,
+    width=281.0,
+    height=35)
 
-entry1_img = PhotoImage(file = f"img_textBox1.png")
+entry1_img = PhotoImage(file=f"img_textBox1.png")
 entry1_bg = canvas.create_image(
     179.5, 84.5,
-    image = entry1_img)
+    image=entry1_img)
 
 entry1 = Entry(
-    bd = 0,
-    bg = "#ffffff",
-    highlightthickness = 0)
+    bd=0,
+    bg="#ffffff",
+    highlightthickness=0,
+    textvariable=v)
 
 entry1.place(
-    x = 40.0, y = 68,
-    width = 281.0,
-    height = 35)
+    x=40.0, y=68,
+    width=281.0,
+    height=35)
 
 window.resizable(True, True)
 window.mainloop()
